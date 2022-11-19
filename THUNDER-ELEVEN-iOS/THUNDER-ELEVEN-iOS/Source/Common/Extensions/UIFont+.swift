8349da7c84deb2c35cae1afc12965bbc0b0686b4
jeongkite <jeong6kite@gmail.com>
2022-11-20 01:43:49 +0900

    [Add] Font and Color

--- THUNDER-ELEVEN-iOS/THUNDER-ELEVEN-iOS/Source/Common/Extensions/UIFont+.swift
+++ THUNDER-ELEVEN-iOS/THUNDER-ELEVEN-iOS/Source/Common/Extensions/UIFont+.swift
@@ -8,5 +8,45 @@
 import UIKit.UIFont
 
 extension UIFont {
-    // 나중에 제플린으로 뽑을 예정...
+
+  class var header1: UIFont {
+    return UIFont(name: "Pretendard-Bold", size: 24.0)!
+  }
+
+  class var header2: UIFont {
+    return UIFont(name: "Pretendard-Bold", size: 22.0)!
+  }
+
+  class var header3: UIFont {
+    return UIFont(name: "Pretendard-Bold", size: 20.0)!
+  }
+
+  class var contents1Bold: UIFont {
+    return UIFont(name: "Pretendard-SemiBold", size: 18.0)!
+  }
+
+  class var contents1Regular: UIFont {
+    return UIFont(name: "Pretendard-Regular", size: 18.0)!
+  }
+
+  class var contents2Bold: UIFont {
+    return UIFont(name: "Pretendard-SemiBold", size: 16.0)!
+  }
+
+  class var contents2Regular: UIFont {
+    return UIFont(name: "Pretendard-Regular", size: 16.0)!
+  }
+
+  class var contents3Bold: UIFont {
+    return UIFont(name: "Pretendard-SemiBold", size: 14.0)!
+  }
+
+  class var contents3Regular: UIFont {
+    return UIFont(name: "Pretendard-Regular", size: 14.0)!
+  }
+
+  class var description1: UIFont {
+    return UIFont(name: "Pretendard-Regular", size: 12.0)!
+  }
+
 }
